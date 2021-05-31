8b2b6b68c58ac62dc0df4171e425f1089a4f251c
Alexander Lercher <allercher@edu.aau.at>
2021-05-31 18:55:51 +0200

    single context changes

--- train.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-source venv/bin/activate
-
-for layer in CallTypeLayer DayTypeLayer EndLocationLayer OriginCallLayer OriginStandLayer StartLocationLayer TaxiIdLayer
-do 
-    python3 train.py $layer
-done
